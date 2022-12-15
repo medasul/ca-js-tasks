0587c28f1e34470c43359fc69d120c3dfba94dd0
medasul <medasulskyte@gmail.com>
2022-12-15 02:58:13 +0200

    edabit 4-5 tasks done

--- week2/function-edabit-tasks/main.js
+++ week2/function-edabit-tasks/main.js
@@ -1,10 +1,6 @@
-//EASY LEVEL;
-
-
-//EASY LEVEL;
 
 console.group('1. https://edabit.com/challenge/XdAR3KohR5w7rjrFg');
-{
+{  console.log('\n-----Sum of Cubes------')
     function sumOfCubes(arr) {
         let a = 0;
         for (i = 0; i < arr.length; i++) {
@@ -24,12 +20,13 @@ console.groupEnd();
 
 console.group('2. https://edabit.com/challenge/h7LTMAFeNz79rXB2Y');
 {
+    console.log('\n-----Find the Largest Numbers in a Group of Arrays------')
     function findLargestNums(arr) {
         let a = [];
         for (let i = 0; i < arr.length; i++) {
-            let maxValue = arr[i].[0];
+            let maxValue = arr[i][0];
             for (let j = 0; j < arr[i].length; j++) {
-                if (arr[i].[j] > maxValue) { maxValue = arr[i].[j]; }
+                if (arr[i][j] > maxValue) { maxValue = arr[i][j]; }
             }
          a[i]=maxValue;}
 
@@ -47,41 +44,70 @@ console.group('2. https://edabit.com/challenge/h7LTMAFeNz79rXB2Y');
 console.groupEnd();
 
 
-
-
-
-//taisoma
 console.group('3. https://edabit.com/challenge/Gz27eKkMcLarzE5Rj');
-{
+{ console.log('\n-----Sort an Array by String Length------')
     function sortByLength(arr) {
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i].length < arr[i - 1].length) {
-                let j = arr[i];
-                arr[i] = arr[i - 1];
-                arr[i - 1] = j;
-                i--;
+        for (let i = 0; i < arr.length; i++) {
+          for (let j = 0; j < arr.length - i - 1; j++) {
+            let a = arr[j]
+            if (arr[j].length > arr[j + 1].length) {
+              arr[j] = arr[j + 1];
+              arr[j + 1] = a;
             }
+          }
         }
-        console.log(arr);
+        return arr;
+      }
 
-    }
-
-    sortByLength(["Google", "Apple", "Microsoft"]);
-    sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"]);
-    sortByLength(['Turing', 'Jung', 'Einstein']);
-    sortByLength(['ddddd', 'cccc', 'bbb', 'a']);
-
+   console.log(sortByLength(["Google", "Apple", "Microsoft"]));
+   console.log(sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"]));
+   console.log(sortByLength(['Turing', 'Jung', 'Einstein']));
+   console.log(sortByLength(['ddddd', 'cccc', 'bbb', 'a']));
 
 }
-
-/*let string1='';
-      let beggining="[ '";
-      let ending="' ]"
-      let betweenTwo="', '"
-       for(let i=0; i<arr.length; i++)
-       { if(i===0){string1+= beggining+arr[i]}
-         else if(i===arr.length-1) {string1+= betweenTwo+arr[i]+ending}
-         else string1+= betweenTwo + arr[i];
-   }
-       console.log(string1);*/
 console.groupEnd();
+
+console.group('4. https://edabit.com/challenge/BFnsRqe8PFvEwcRNt ');
+{ console.log('\n-----Pair Management------')
+    
+function makePair(a, b) {
+	let arr = [a, b]
+	return arr;
+}
+
+console.log(makePair(1, 2)) //➞ [1, 2]
+console.log(makePair(51, 21)) //➞ [51, 21]
+console.log(makePair(512124, 215)) //➞ [512124, 215]
+}
+console.groupEnd();
+
+console.group('5. https://edabit.com/challenge/Q3n42rEWanZSTmsJm  ');
+{ console.log('\n-----Find the Smallest and Biggest Numbers------')
+    
+function minMax(arr) {
+    let min = max = arr[0];
+    let arr1 = [];
+    for(let i = 1;i<arr.length;i++) {
+      if(arr[i]>max) max = arr[i];
+      if(arr[i]<min) min = arr[i];
+    }
+    arr1[0]=min;
+    arr1[1]=max;
+    return arr1;
+  }
+
+console.log(minMax([1, 2, 3, 4, 5])) //➞ [1, 5]
+console.log(minMax([2334454, 5])) // ➞ [5, 2334454]
+console.log(minMax([1])) //➞ [1, 1]
+}
+console.groupEnd();
+
+/*
+
+console.group(' ');
+{ console.log('\n-----Find the Smallest and Biggest Numbers------')
+    
+
+}
+console.groupEnd();
+*/
