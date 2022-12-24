b70cd1dc790b560d5f7b05e6b4e3c2fab2a3f6e0
medasul <medasulskyte@gmail.com>
2022-12-24 07:12:28 +0200

    function-tasks-3 : tasks 16-21 done

--- week 3/function-tasks-3.js
+++ week 3/function-tasks-3.js
@@ -271,7 +271,7 @@ console.group("-------functions-tasks-3.js [užduotys 1-27]------");
         function letterACount(str) {
             let ans = 0;
 
-            for (const ch of str) {
+            for (let ch of str) {
                 if (ch.toLowerCase() === 'a') { ans++; }
             }
 
@@ -288,19 +288,24 @@ console.group("-------functions-tasks-3.js [užduotys 1-27]------");
     console.groupEnd();
     console.log();
 
-    /*
+
     console.group("16. Sukurkite funkciją, kuri grąžina bet kokios raidės kiekį žodyje");
     {
         function letterCount(str, searchLetter) {
-            // code ...
+            let ans = 0;
+            for (let ch of str) {
+                if (ch.toLowerCase() === searchLetter) { ans++; }
+            }
+
+            return ans;
         }
-        // console.log('---');
-        // console.log({
-        //   'labas, a': letterCount('labas', 'a'),
-        //   'kempės, k': letterCount('kempės', 'k'),
-        //   '123123, z': letterCount('123123', 'z'),
-        // })
-        // console.log('---');
+        console.log('---');
+        console.log({
+            'labas, a': letterCount('labas', 'a'),
+            'kempės, k': letterCount('kempės', 'k'),
+            '123123, z': letterCount('123123', 'z'),
+        })
+        console.log('---');
     }
     console.groupEnd();
     console.log();
@@ -308,15 +313,17 @@ console.group("-------functions-tasks-3.js [užduotys 1-27]------");
     console.group("17. Sukurkite funkciją, kuri grąžina pirmos (nuo kairės pusės) 'a' raidės vietą žodyje");
     {
         function lastIndexOfLetterA(str) {
-            // code ...
+
+            if (str.lastIndexOf('a') > -1) { return str.lastIndexOf('a'); }
+            else { return (` a raidės nėra `); }
         }
-        // console.log('---');
-        // console.log({
-        //   'labas': lastIndexOfLetterA('labas'),
-        //   'kempės': lastIndexOfLetterA('kempės'),
-        //   '123123': lastIndexOfLetterA('123123'),
-        // })
-        // console.log('---');
+        console.log('---');
+        console.log({
+            'labas': lastIndexOfLetterA('labas'),
+            'kempės': lastIndexOfLetterA('kempės'),
+            '123123': lastIndexOfLetterA('123123'),
+        })
+        console.log('---');
     }
     console.groupEnd();
     console.log();
@@ -324,15 +331,17 @@ console.group("-------functions-tasks-3.js [užduotys 1-27]------");
     console.group("18. Sukurkite funkciją, kuri grąžina pirmos (nuo kairės pusės) vartotojo perduotos raidės vietą žodyje");
     {
         function lastIndexOfLetter(str, searchLetter) {
-            // code ...
+            if (str.lastIndexOf(searchLetter) === -1) { return ("tokios raidės nėra"); }
+            else { return (str.lastIndexOf(searchLetter)); }
         }
-        // console.log('---');
-        // console.log({
-        //   'labas, a': lastIndexOfLetter('labas', 'a'),
-        //   'kempės, k': lastIndexOfLetter('kempės', 'k'),
-        //   '123123, z': lastIndexOfLetter('123123', 'z'),
-        // })
-        // console.log('---');
+
+        console.log('---');
+        console.log({
+            'labas, a': lastIndexOfLetter('labas', 'a'),
+            'kempės, k': lastIndexOfLetter('kempės', 'k'),
+            '123123, z': lastIndexOfLetter('123123', 'z'),
+        })
+        console.log('---');
     }
     console.groupEnd();
     console.log();
@@ -340,15 +349,22 @@ console.group("-------functions-tasks-3.js [užduotys 1-27]------");
     console.group("19. Sukurkite funkciją, kuri grąžina indeksų masyvą su visais 'a' raidės pasikartojimais žodyje");
     {
         function indexesOfLetterA(str) {
-            // code ...
+            const array = [];
+            for (let i = 0; i <= str.length - 1; i++) {
+                if (str[i].toLowerCase() === 'a') {
+                    array.push(i);
+                }
+            }
+
+            return array;
         }
-        // console.log('---');
-        // console.log({
-        //   'labas': indexesOfLetterA('labas'),
-        //   'kempės': indexesOfLetterA('kempės'),
-        //   '123123': indexesOfLetterA('123123'),
-        // })
-        // console.log('---');
+        console.log('---');
+        console.log({
+            'labas': indexesOfLetterA('labas'),
+            'kempės': indexesOfLetterA('kempės'),
+            '123123': indexesOfLetterA('123123'),
+        })
+        console.log('---');
     }
     console.groupEnd();
     console.log();
@@ -356,15 +372,22 @@ console.group("-------functions-tasks-3.js [užduotys 1-27]------");
     console.group("20. Sukurkite funkciją, kuri grąžina indeksų masyvą su visais vartotojo įvestos raidės pasikartojimais žodyje");
     {
         function indexesOfLetter(str, searchLetter) {
-            // code ...
+            const array1 = [];
+            for (let i = 0; i <= str.length - 1; i++) {
+                if (str[i].toLowerCase() === searchLetter) {
+                    array1.push(i);
+                }
+            }
+
+            return array1;
         }
-        // console.log('---');
-        // console.log({
-        //   'labas, a': indexesOfLetter('labas', 'a'),
-        //   'kempės, m': indexesOfLetter('kempės', 'm'),
-        //   '123123, 2': indexesOfLetter('123123', '2'),
-        // })
-        // console.log('---');
+        console.log('---');
+        console.log({
+            'labas, a': indexesOfLetter('labas', 'a'),
+            'kempės, m': indexesOfLetter('kempės', 'm'),
+            '123123, 2': indexesOfLetter('123123', '2'),
+        })
+        console.log('---');
     }
     console.groupEnd();
     console.log();
@@ -372,114 +395,119 @@ console.group("-------functions-tasks-3.js [užduotys 1-27]------");
     console.group("21. Sukurkite funkciją, kuri ištrintų pirmą surastą 'a' raidę žodyje ir grąžintų pakeistą žodį");
     {
         function removeFirstLetterA(str) {
-            // code ...
+            const indexOfFirstA = str.toLowerCase().indexOf('a');
+
+            if (indexOfFirstA !== -1) {return (str.substring(0, indexOfFirstA) + str.substring(indexOfFirstA + 1))
+            }
+
+            else {return str};
         }
-        // console.log('---');
-        // console.log({
-        //   'labas': removeFirstLetterA('labas'),
-        //   'kempiniukas': removeFirstLetterA('kempiniukas'),
-        //   '123123': removeFirstLetterA('123123'),
-        // })
-        // console.log('---');
+        console.log('---');
+        console.log({
+            'labas': removeFirstLetterA('labas'),
+            'kempiniukas': removeFirstLetterA('kempiniukas'),
+            '123123': removeFirstLetterA('123123'),
+        })
+        console.log('---');
     }
     console.groupEnd();
     console.log();
-
-    console.group("22. Sukurkite funkciją, kuri ištrintų paskutinę surastą 'a' raidę žodyje ir grąžintų pakeistą žodį");
-    {
-        function removeLastLetterA(str) {
-            // code ...
+    /*
+        console.group("22. Sukurkite funkciją, kuri ištrintų paskutinę surastą 'a' raidę žodyje ir grąžintų pakeistą žodį");
+        {
+            function removeLastLetterA(str) {
+                // code ...
+            }
+            // console.log('---');
+            // console.log({
+            //   'labas': removeLastLetterA('labas'),
+            //   'kempiniukas': removeLastLetterA('kempiniukas'),
+            //   '123123': removeLastLetterA('123123'),
+            // })
+            // console.log('---');
         }
-        // console.log('---');
-        // console.log({
-        //   'labas': removeLastLetterA('labas'),
-        //   'kempiniukas': removeLastLetterA('kempiniukas'),
-        //   '123123': removeLastLetterA('123123'),
-        // })
-        // console.log('---');
-    }
-    console.groupEnd();
-    console.log();
-
-    console.group("23. Sukurkite funkciją, kuri ištrintų visus 'a' raidės pasikartojimus žodyje");
-    {
-        function removeAllOccurencesOfLetterA(str) {
-            // code ...
+        console.groupEnd();
+        console.log();
+    
+        console.group("23. Sukurkite funkciją, kuri ištrintų visus 'a' raidės pasikartojimus žodyje");
+        {
+            function removeAllOccurencesOfLetterA(str) {
+                // code ...
+            }
+            // console.log('---');
+            // console.log({
+            //   'labas': removeAllOccurencesOfLetterA('labas'),
+            //   'kempiniukas': removeAllOccurencesOfLetterA('kempiniukas'),
+            //   '123123': removeAllOccurencesOfLetterA('123123'),
+            // })
+            // console.log('---');
         }
-        // console.log('---');
-        // console.log({
-        //   'labas': removeAllOccurencesOfLetterA('labas'),
-        //   'kempiniukas': removeAllOccurencesOfLetterA('kempiniukas'),
-        //   '123123': removeAllOccurencesOfLetterA('123123'),
-        // })
-        // console.log('---');
-    }
-    console.groupEnd();
-    console.log();
-
-    console.group("24. Sukurkite funkciją, kuri ištrintų visus, vartotojo įvestos raidės pasikartojimus žodyje");
-    {
-        function removeAllOccurencesOfLetter(str, letter) {
-            // code ...
+        console.groupEnd();
+        console.log();
+    
+        console.group("24. Sukurkite funkciją, kuri ištrintų visus, vartotojo įvestos raidės pasikartojimus žodyje");
+        {
+            function removeAllOccurencesOfLetter(str, letter) {
+                // code ...
+            }
+            // console.log('---');
+            // console.log({
+            //   'labas, a': removeAllOccurencesOfLetter('labas', 'a'),
+            //   'kempiniukas, i': removeAllOccurencesOfLetter('kempiniukas', 'i'),
+            //   '123123, 3': removeAllOccurencesOfLetter('123123', '3'),
+            // })
+            // console.log('---');
         }
-        // console.log('---');
-        // console.log({
-        //   'labas, a': removeAllOccurencesOfLetter('labas', 'a'),
-        //   'kempiniukas, i': removeAllOccurencesOfLetter('kempiniukas', 'i'),
-        //   '123123, 3': removeAllOccurencesOfLetter('123123', '3'),
-        // })
-        // console.log('---');
-    }
-    console.groupEnd();
-    console.log();
-
-    // 25. Sukurkite funkciją, kuri pirmu parametru priimtų žodį, o antruoju - masyvą su raidėmis.
-    //  Ši funkcija turi žodyje ištrinti visas raides, kurios perduotos antruoju parametru.
-    //  Atlikus veiksmus, grąžinti pakeistą žodį
-    console.group("25. Sukurkite funkciją, kuri pirmu parametru priimtų žodį, o antruoju - masyvą su raidėmis.");
-    {
-        function filterLetters(str, lettersToRemove) {
-            // code ...
+        console.groupEnd();
+        console.log();
+    
+        // 25. Sukurkite funkciją, kuri pirmu parametru priimtų žodį, o antruoju - masyvą su raidėmis.
+        //  Ši funkcija turi žodyje ištrinti visas raides, kurios perduotos antruoju parametru.
+        //  Atlikus veiksmus, grąžinti pakeistą žodį
+        console.group("25. Sukurkite funkciją, kuri pirmu parametru priimtų žodį, o antruoju - masyvą su raidėmis.");
+        {
+            function filterLetters(str, lettersToRemove) {
+                // code ...
+            }
+            // console.log('---');
+            // const str = filterLetters('Brangakmienio paveikslas', ['a', 'i']);
+            // console.log(str);
+            // console.log('---');
         }
-        // console.log('---');
-        // const str = filterLetters('Brangakmienio paveikslas', ['a', 'i']);
-        // console.log(str);
-        // console.log('---');
-    }
-    console.groupEnd();
-    console.log();
-
-    console.group("26. Sukurkite funkciją, kuri pirmu parametrų priimą sakinį, kiekvieną tarpą pakeičia brūkšneliu ir grąžina pakeistą sakinį");
-    {
-        function replaceSpaceWithDash(str) {
-            // code ...
+        console.groupEnd();
+        console.log();
+    
+        console.group("26. Sukurkite funkciją, kuri pirmu parametrų priimą sakinį, kiekvieną tarpą pakeičia brūkšneliu ir grąžina pakeistą sakinį");
+        {
+            function replaceSpaceWithDash(str) {
+                // code ...
+            }
+            // console.log('---');
+            // console.log({
+            //   'viens du trys': replaceSpaceWithDash('viens du trys'),
+            //   'as tave myliu': replaceSpaceWithDash('as tave myliu'),
+            //   'Bairis seniuk': replaceSpaceWithDash('Bairis seniuk'),
+            // })
+            // console.log('---');
         }
-        // console.log('---');
-        // console.log({
-        //   'viens du trys': replaceSpaceWithDash('viens du trys'),
-        //   'as tave myliu': replaceSpaceWithDash('as tave myliu'),
-        //   'Bairis seniuk': replaceSpaceWithDash('Bairis seniuk'),
-        // })
-        // console.log('---');
-    }
-    console.groupEnd();
-    console.log();
-
-    console.group("27. Sukurkite funkciją, kuri pirmu parametru priima sakinį, ir kiekvieno žodžio pirmają raidę padaro didžiąja");
-    {
-        function capitalize(str) {
-            // code ...
+        console.groupEnd();
+        console.log();
+    
+        console.group("27. Sukurkite funkciją, kuri pirmu parametru priima sakinį, ir kiekvieno žodžio pirmają raidę padaro didžiąja");
+        {
+            function capitalize(str) {
+                // code ...
+            }
+            // console.log('---');
+            // console.log({
+            //   'viens du trys': capitalize('viens du trys'),
+            //   'as tave myliu': capitalize('as tave myliu'),
+            //   'Bairis seniuk': capitalize('Bairis seniuk'),
+            // })
+            // console.log('---');
         }
-        // console.log('---');
-        // console.log({
-        //   'viens du trys': capitalize('viens du trys'),
-        //   'as tave myliu': capitalize('as tave myliu'),
-        //   'Bairis seniuk': capitalize('Bairis seniuk'),
-        // })
-        // console.log('---');
-    }
-    console.groupEnd();
-
-*/
+        console.groupEnd();
+    
+    */
 }
 console.groupEnd();
